6a6741492521b771d1d6f45ba0b234a0926506e8
Dulya Manodara <dulyacsm15@gmail.com>
2024-09-22 21:26:02 +1000

    ImagePicker fix: setImage using URI correctly

--- client/components/UploadModal.js
+++ client/components/UploadModal.js
@@ -4,56 +4,72 @@ import * as ImagePicker from 'expo-image-picker';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const UploadModal = () => {
-    const [image, setImage] = useState(null);
+  const [image, setImage] = useState('');
+  const [havePermissions, setHavePermissions] = useState(false)
 
-    useEffect(() => {
-      (async () => {
-        if (Platform.OS !== 'web') {
-          const libraryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
-          if (libraryStatus.status !== 'granted') {
-            alert('Sorry, we need camera roll permissions to make this work!');
-          }
-
-          const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
-          if (cameraStatus.status !== 'granted') {
-            alert('Sorry, we need camera permissions to make this work!');
-          }
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const libraryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (libraryStatus.status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
         }
-      })();
-    }, []);
 
-    const pickImage = async () => {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
-      });
+        const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+        if (cameraStatus.status !== 'granted') {
+          alert('Sorry, we need camera permissions to make this work!');
+        }
 
-      if (!result.cancelled) {
-        setImage(result.uri);
+        if (cameraStatus.status === 'granted' && libraryStatus.status === 'granted') {
+          // debugging: alert('Perfect, all permissions set!');
+          setHavePermissions(true)
+        }
       }
-    };
+    })();
+  }, []);
 
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={pickImage}>
-            <View style={styles.buttonTextContainer}>
-                <MaterialCommunityIcons name='image-outline' size={30} color="#fff"/>
-                <Text style={styles.buttonText}>Upload image</Text>
-            </View>
-        </TouchableOpacity>
-        {image && <Image source={{ uri: image }} style={styles.image} />}
-        <TouchableOpacity style={styles.button} onPress={pickImage}>
-            <View style={styles.buttonTextContainer}>
-                <MaterialCommunityIcons name='camera-outline' size={30} color="#fff"/>
-                <Text style={styles.buttonText}>Scan image</Text>
-            </View>
-        </TouchableOpacity>
-        {image && <Image source={{ uri: image }} style={styles.image} />}
-      </View>
-    );
+  const pickImage = async () => {
+    try {
+      if (havePermissions) {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+  
+        console.log(result);
+  
+        if (!result.cancelled) {
+          setImage(result.assets[0].uri);
+        }
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
   };
+
+  return (
+    <View style={styles.container}>
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+
+      <TouchableOpacity style={styles.button} onPress={pickImage}>
+          <View style={styles.buttonTextContainer}>
+              <MaterialCommunityIcons name='image-outline' size={30} color="#fff"/>
+              <Text style={styles.buttonText}>Upload image</Text>
+          </View>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.button} onPress={pickImage}>
+          <View style={styles.buttonTextContainer}>
+              <MaterialCommunityIcons name='camera-outline' size={30} color="#fff"/>
+              <Text style={styles.buttonText}>Scan image</Text>
+          </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
   export default UploadModal;
 
