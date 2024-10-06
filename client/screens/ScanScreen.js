8589aae1cc57c435199297ba35c89a9733631c45
Dulya Manodara <dulyacsm15@gmail.com>
2024-10-06 22:09:07 +1100

    Save uploaded images to database

--- client/screens/ScanScreen.js
+++ client/screens/ScanScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import UploadModal from '../components/UploadModal-Backup';
+import UploadModal from '../components/UploadModal';
 
 export default function ScanScreen() {
   return (
