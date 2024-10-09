6e315c9522337ef0f72baedea94bec2978c118aa
dcsm15 <dulyacsm15@gmail.com>
2024-10-09 12:09:56 +1100

    Issue 06: Save uploaded images to database (#26)
    * Save uploaded images to database
    
    * Rebase to resolve conflicts with dev

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const OpenAI = require('openai'); // Import OpenAI SDK
+const multer = require('multer'); // Import multer middleware for file upload
+const { v4: uuidv4 } = require('uuid');
+const { Storage } = require('@google-cloud/storage'); // Import GCS SDK
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -83,7 +87,89 @@ app.post('/chat', async (req, res) => {
   }
 });
 
+// User schema and model
+const speciesSchema = new mongoose.Schema({
+  // userId: { type: _id, required: true, unique: true },
+  name: {type: String, required: true, unique: true},
+  imageUrl: {type: String, required: true},
+  contentType: {type: String, required: true},
+  createdAt: { type: Date, required: true },
+});
+
+const Species = mongoose.model('Species', speciesSchema);
+
+// Ensure that keys file for the google cloud storage service account exists
+const gcsKeyPath = 'camprisma-gcs.json'
+const storage = new Storage({ keyFilename: path.join(__dirname, gcsKeyPath) });
+const bucket = storage.bucket('bucket-quickstart-camprismatest');
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.post('/upload', upload.single('image'), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({success: false, message: 'No file provided.'});
+  }
+
+  console.log('File received by server!');
+
+  // Create a new blob in the bucket and upload the file data
+  const folderName = 'uploads';
+  const destination = `${folderName}/${req.file.originalname}`;
+  const blob = bucket.file(destination);
+  const blobStream = blob.createWriteStream({
+    resumable: false,
+    contentType: req.file.mimetype,
+  });
+
+  blobStream.on('error', (err) => {
+    console.error(err);
+    res.status(500).send(err);
+  });
+
+  blobStream.on('finish', () => {
+    // res.status(200).send('File uploaded successfully.');
+    console.log('File uploaded to GCS successfully!');
+  });
+
+  blobStream.end(req.file.buffer); // Upload the file buffer
+
+  const image = new Species({
+    // using uuid here to create a unique name for now
+    // until the computer vision is ready to be integrated
+    // name: `${uuidv4()}.${req.file.mimetype.split('/')[1]}`,
+    name: `${uuidv4()}`,
+    imageUrl: `https://storage.googleapis.com/bucket-quickstart-camprismatest/uploads/${req.file.originalname}`,
+    contentType: req.file.mimetype,
+    createdAt: new Date(),
+  });
+  console.log('Entry created for image saving:\n', image.toJSON());
+
+  try {
+    await image.save();
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({success: false, message: error.message});
+  }
+  return res.status(201).json({success: true, message: 'Storage object created successfully.'});
+});
+
+app.get('/species', async (req, res) => {
+  try {
+    const species = await Species.find({});
+    res.json(species);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get('/posts', async (req, res) => {
+  try {
+    const posts = await Species.find({});
+    res.json(posts);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 app.listen(8080, () => {
-  console.log('Server running on http://192.168.50.169:8080');
+  console.log('Server running on http://192.168.0.110:8080');
 });
